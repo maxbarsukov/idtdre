224c08b4168e3d54f1ec88117d4fd158f8a354a9
maxbarsukov <maximbarsukov@bk.ru>
2021-01-09 20:22:33 +0300

    Bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("bootstrap")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

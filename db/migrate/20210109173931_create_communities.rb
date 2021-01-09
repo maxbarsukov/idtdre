fc7c9a2ab7cc620a7284024611f142d8983053b6
maxbarsukov <maximbarsukov@bk.ru>
2021-01-09 20:47:48 +0300

    Header and DB updated

--- db/migrate/20210109173931_create_communities.rb
+++ db/migrate/20210109173931_create_communities.rb
@@ -1,6 +1,11 @@
 class CreateCommunities < ActiveRecord::Migration[6.1]
   def change
     create_table :communities do |t|
+      t.references :account
+      t.string :name
+      t.string :url
+      t.text :rules
+      t.integer :total_members
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_170244) do
+ActiveRecord::Schema.define(version: 2021_01_09_173931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 2021_01_09_170244) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "communities", force: :cascade do |t|
+    t.bigint "account_id"
+    t.string "name"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_communities_on_account_id"
   end
 
 end

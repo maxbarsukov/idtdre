7652dc143fc8e48c525fa566cda92d0c48e2420c
maxbarsukov <maximbarsukov@bk.ru>
2021-01-10 12:05:41 +0300

    Profile pge added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
 
-  get 'u/:username' => 'public#index', as: :profile
+  get 'u/:username' => 'public#profile', as: :profile
 
   resources :communities do
     resources :posts

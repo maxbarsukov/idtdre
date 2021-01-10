9e45fbc7d107597d2c4a5483372218bb38470d19
maxbarsukov <maximbarsukov@bk.ru>
2021-01-10 12:39:20 +0300

    Community fixed

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,7 @@
 class Community < ApplicationRecord
   belongs_to :account
-  has_many :posts
-
   validates_presence_of :url, :name, :rules
+  has_many :posts
+  has_many :subscriptions
+  has_many :subscribers, through: :subscription, source: :account
 end

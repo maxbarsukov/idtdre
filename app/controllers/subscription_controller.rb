586c8802a2a07fa36a651221b4c6cecada00c7f4
maxbarsukov <maximbarsukov@bk.ru>
2021-01-10 17:15:46 +0300

    Subscriptions updated

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -1,3 +1,15 @@
 class SubscriptionController < ApplicationController
+  def create
+    @subscription = Subscription.new(subscription_params)
+    @subscription.account_id = current_account.id
+    @subscription.save
 
+    redirect_to community_path(@subscription.community_id)
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:community_id)
+  end
 end

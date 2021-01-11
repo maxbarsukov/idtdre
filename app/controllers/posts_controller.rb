65b2d22bc82eba3d6012ca46c697c5093b889750
maxbarsukov <maximbarsukov@bk.ru>
2021-01-11 19:51:02 +0300

    Auth acontrol added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_account!, except: [:index, :show]
   before_action :set_post, only: [:show]
+  before_action :auth_subscriber, only: [:new]
 
   def index
     @posts = Post.all
@@ -31,6 +32,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def auth_subscriber
+    unless Subscription.where(community_id: params[:community_id], account_id: current_account.id).any?
+      redirect_to root_path, flash: { danger: "You are not authorized to view this page"}
+    end
   end
 
   def post_values

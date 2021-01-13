dda063cf0f5182ca6a002554eebd14cf22f2bf79
maxbarsukov <maximbarsukov@bk.ru>
2021-01-13 20:48:47 +0300

    Popular posts sorts by score

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,8 @@
 class PublicController < ApplicationController
   def index
     @communities = Community.all.limit(5)
-    @posts = Post.order(id: :desc).limit(20)
+    @posts = Post.where("created_at >= ?", 1.day.ago).limit(30).sort_by{ |p| p.score }.reverse
+    @vote = Vote.new
   end
   
   def profile

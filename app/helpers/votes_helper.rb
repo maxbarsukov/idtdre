e396f86bc85de47cc33f097e1400a2001812c88a
maxbarsukov <maximbarsukov@bk.ru>
2021-01-13 20:24:36 +0300

    Votes helper updated

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,13 +1,9 @@
 module VotesHelper
   def is_upvoted post
-    if account_signed_in?
-      current_account.upvoted_posts_ids.include? post.id
-    end
+    account_signed_in? && current_account.upvoted_posts_ids.include?(post.id) ? "active" : ""
   end
 
   def is_downvoted post
-    if account_signed_in?
-      current_account.downvoted_posts_ids.include? post.id
-    end
+    account_signed_in? && current_account.downvoted_posts_ids.include?(post.id) ? "active" : ""
   end
 end
